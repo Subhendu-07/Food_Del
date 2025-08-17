27ccb81a3f3542f31cc8b3ce47bdc174a1a40e12
Subhendu-07 <mandalsubhendu243@gmail.com>
2025-08-17 20:39:12 +0530

    edit db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config(); 
 
 export const connectDB = async () => {
-    await mongoose.connect(
-      "mongodb+srv://Subhendu:Subhendu243@cluster0.jsabokt.mongodb.net/food_delivery_webpage"
-    );
-    console.log("DB Connected");
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("DB Connected");
+    } catch (error) {
+        console.error("DB Connection Error:", error);
+        process.exit(1); 
+    }
 };
